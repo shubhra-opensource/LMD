b9bf57db737158cfda29a08df602cf2b49dc6e6a
shubhra-opensource <shubhraprakash279@gmail.com>
2022-09-09 14:42:36 +0530

    Initial Commit

---
+++ R/square_val.R
@@ -0,0 +1,13 @@
+#' Title
+#'
+#' @param x: A numeric value to be squared
+#'
+#' @return y: # number
+#' @export
+#'
+#' @examples
+#' square_val(2)
+square_val <- function(x) {
+  return(y=x**2)
+
+}
